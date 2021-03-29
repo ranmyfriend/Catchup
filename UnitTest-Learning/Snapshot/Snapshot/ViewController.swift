e4cf7b14615dea9b7214a76a17354eebe4cdd9a4
ranmyfriend <ranjithkumar2010a@live.com>
2021-03-29 12:29:24 +0530

    Chapter 16: Snapshot Testing chapter completed
    >> Using FBSnapShotCase dependency to capture the screenshot of screen
    >> Using Kaleidoscope Mac tool to compare the screenshot and find the difference

---
+++ UnitTest-Learning/Snapshot/Snapshot/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Snapshot
+//
+//  Created by BKS-GGS on 28/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ UnitTest-Learning/Snapshot/SnapshotTests/ViewControllerSnapshotTests.swift
@@ -0,0 +1,18 @@
+@testable import Snapshot
+import FBSnapshotTestCase
+
+final class ViewControllerSnapshotTests: FBSnapshotTestCase {
+
+    override func setUp() {
+        super.setUp()
+        recordMode = false
+    }
+    
+    
+    func test_example() {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let sut: ViewController = storyboard.instantiateViewController(identifier: String(describing: ViewController.self))
+        FBSnapshotVerifyViewController(sut)
+    }
+    
+}
