5579a83557159c77f546cfec1e3935cd986a3966
Ranjith <Ranjith.Rajendiran-ext@bshg.com>
2021-07-29 12:24:18 +0530

    Part 14 completed:SearchViewController and results controller designed

--- Spotify/Spotify/Controllers/Core/SearchViewController.swift
+++ Spotify/Spotify/Controllers/Core/SearchViewController.swift
@@ -8,9 +8,89 @@
 import UIKit
 
 class SearchViewController: UIViewController {
+  
+  let searchController: UISearchController = {
+    let searchController = UISearchController(searchResultsController: SearchResultsViewController())
+    searchController.searchBar.placeholder = "Songs, Artists, Albums"
+    searchController.searchBar.searchBarStyle = .minimal
+    searchController.definesPresentationContext = true
+    return searchController
+  }()
 
+  private let collectionView: UICollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewCompositionalLayout(sectionProvider: { _, _ in
+    
+    let item = NSCollectionLayoutItem(
+      layoutSize: NSCollectionLayoutSize(
+        widthDimension: .fractionalWidth(1),
+        heightDimension: .fractionalHeight(1)
+      )
+    )
+    
+    item.contentInsets = NSDirectionalEdgeInsets(top: 2, leading: 7, bottom: 2, trailing: 7)
+    
+    let group = NSCollectionLayoutGroup.horizontal(
+      layoutSize: NSCollectionLayoutSize(
+        widthDimension: .fractionalWidth(1),
+        heightDimension: .absolute(150)
+      ),
+      subitem: item,
+      count: 2
+    )
+    
+    group.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 0, bottom: 10, trailing: 0)
+
+    
+    return NSCollectionLayoutSection(group: group)
+  }))
+  
+  // Mark: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+      searchController.searchResultsUpdater = self
+      navigationItem.searchController = searchController
+      view.addSubview(collectionView)
+      collectionView.register(GenreCollectionViewCell.self, forCellWithReuseIdentifier: GenreCollectionViewCell.identifier)
+      collectionView.delegate = self
+      collectionView.dataSource = self
+      collectionView.backgroundColor = .systemBackground
     }
+  
+  override func viewDidLayoutSubviews() {
+    super.viewDidLayoutSubviews()
+    collectionView.frame = view.bounds
+  }
+}
+
+
+extension SearchViewController: UISearchResultsUpdating {
+  func updateSearchResults(for searchController: UISearchController) {
+    guard let resultsController = searchController.searchResultsController as? SearchResultsViewController,
+          let query = searchController.searchBar.text,
+          !query.trimmingCharacters(in: .whitespaces).isEmpty else {
+      return
+    }
+    // res
+    //Perform search
+    print("query:\(query)")
+  }
+}
+
+
+extension SearchViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+  func numberOfSections(in collectionView: UICollectionView) -> Int {
+    return 1
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return 20
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: GenreCollectionViewCell.identifier, for: indexPath) as? GenreCollectionViewCell else {
+      return UICollectionViewCell()
+    }
+    cell.configure(with: "Rock")
+    return cell
+  }
 }
---
+++ Spotify/Spotify/Views/GenreCollectionViewCell.swift
@@ -0,0 +1,70 @@
+//
+//  GenreCollectionViewCell.swift
+//  Spotify
+//
+//  Created by BKS-GGS on 29/07/21.
+//
+
+import UIKit
+
+class GenreCollectionViewCell: UICollectionViewCell {
+  
+  static let identifier = "GenreCollectionViewCell"
+  
+  private let imageView: UIImageView = {
+    let imageView = UIImageView()
+    imageView.contentMode = .scaleAspectFit
+    imageView.tintColor = .white
+    imageView.image = UIImage(systemName: "music.quarternote.3", withConfiguration: UIImage.SymbolConfiguration(pointSize: 50, weight: .regular))
+    return imageView
+  }()
+  
+  private let label: UILabel = {
+    let label = UILabel()
+    label.textColor = .white
+    label.font = .systemFont(ofSize: 22, weight: .semibold)
+    return label
+  }()
+  
+  private let colors: [UIColor] = [
+    .systemPink,
+    .systemBlue,
+    .systemPurple,
+    .systemOrange,
+    .systemGreen,
+    .systemYellow,
+    .systemTeal
+  ]
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    
+    imageView.layer.cornerRadius = 8
+    imageView.layer.masksToBounds = true
+    contentView.addSubview(label)
+    contentView.addSubview(imageView)
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError()
+  }
+  
+  override func prepareForReuse() {
+    super.prepareForReuse()
+    label.text = nil
+  }
+  
+  override func layoutSubviews() {
+    super.layoutSubviews()
+    
+    label.frame = CGRect(x: 10, y: contentView.height/2, width: contentView.width-20, height: contentView.height/2)
+    imageView.frame = CGRect(x: contentView.width/2, y: 0, width: contentView.width/2, height: contentView.height/2)
+    
+  }
+  
+  func configure(with title: String) {
+    label.text = title
+    contentView.backgroundColor = colors.randomElement()
+  }
+    
+}
