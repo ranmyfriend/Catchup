9b288b13e84f84cc81dc2147502ac7b90f114556
Ranjith <Ranjith.Rajendiran-ext@bshg.com>
2021-08-25 19:56:18 +0530

    GraphQL Country project created and apollo 3rd party enabled

---
+++ GraphQLCountry/GraphQLCountry/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GraphQLCountry
+//
+//  Created by BKS-GGS on 25/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GraphQLCountry/GraphQLCountry/GraphQLCountryApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraphQLCountryApp.swift
+//  GraphQLCountry
+//
+//  Created by BKS-GGS on 25/08/21.
+//
+
+import SwiftUI
+
+@main
+struct GraphQLCountryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
