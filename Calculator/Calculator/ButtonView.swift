2c2b366594491f29c02d456d78912ac62301bcac
Ranjith <Ranjith.Rajendiran-ext@bshg.com>
2021-08-30 23:21:54 +0530

    Binding button text are changed into parent txtfield

--- Calculator/Calculator/ButtonView.swift
+++ Calculator/Calculator/ButtonView.swift
@@ -9,20 +9,22 @@ import SwiftUI
 
 struct ButtonView: View {
   @Binding var title: String
-    var body: some View {
-      Button(title) {
-        
-      }
-      .foregroundColor(.black)
-      .font(Font.system(size: 25, weight: .semibold))
-      .frame(width: 50, height: 50)
-      .padding(EdgeInsets(top: 5, leading: 5, bottom: 5, trailing: 5))
-      .border(Color.black, width: 2)
+  @Binding var value: String
+  
+  var body: some View {
+    Button(title) {
+      value = title
     }
+    .foregroundColor(.black)
+    .font(Font.system(size: 25, weight: .semibold))
+    .frame(width: 50, height: 50)
+    .padding(EdgeInsets(top: 5, leading: 5, bottom: 5, trailing: 5))
+    .border(Color.black, width: 2)
+  }
 }
 
 struct ButtonView_Previews: PreviewProvider {
-    static var previews: some View {
-      ButtonView(title: .constant("9"))
-    }
+  static var previews: some View {
+    ButtonView(title: .constant("0"), value: .constant("0"))
+  }
 }
--- Calculator/Calculator/ContentView.swift
+++ Calculator/Calculator/ContentView.swift
@@ -8,8 +8,7 @@
 import SwiftUI
 
 struct Number {
-  let title: String
-  let value: String
+  var value: String
 }
 
 struct ContentView: View {
@@ -18,38 +17,40 @@ struct ContentView: View {
   var body: some View {
     ScrollView {
       VStack(alignment: .leading) {
-        TextField("0", text: $input)
+        TextField("", text: $input)
           .font(Font.system(size: 25, weight: .semibold))
           .frame(width: .infinity, height: 55)
           .foregroundColor(.white)
           .background(Color.gray)
           .multilineTextAlignment(.trailing)
-          
+        
         Spacer(minLength: 25)
         
         HStack {
-          ButtonView(title: .constant("7"))
-          ButtonView(title: .constant("8"))
-          ButtonView(title: .constant("9"))
-          ButtonView(title: .constant("+"))
+          ButtonView(title: .constant("7"), value: $input)
+            .accessibility(label: Text("7 number"))
+            .accessibility(value: Text("7"))
+          ButtonView(title: .constant("8"), value: $input)
+          ButtonView(title: .constant("9"), value: $input)
+          ButtonView(title: .constant("+"), value: $input)
         }
         HStack {
-          ButtonView(title: .constant("4"))
-          ButtonView(title: .constant("5"))
-          ButtonView(title: .constant("2"))
-          ButtonView(title: .constant("-"))
+          ButtonView(title: .constant("4"), value: $input)
+          ButtonView(title: .constant("5"), value: $input)
+          ButtonView(title: .constant("6"), value: $input)
+          ButtonView(title: .constant("-"), value: $input)
         }
         HStack {
-          ButtonView(title: .constant("1"))
-          ButtonView(title: .constant("3"))
-          ButtonView(title: .constant("2"))
-          ButtonView(title: .constant("*"))
+          ButtonView(title: .constant("1"), value: $input)
+          ButtonView(title: .constant("2"), value: $input)
+          ButtonView(title: .constant("3"), value: $input)
+          ButtonView(title: .constant("*"), value: $input)
         }
         HStack {
-          ButtonView(title: .constant("/"))
-          ButtonView(title: .constant("0"))
-          ButtonView(title: .constant("."))
-          ButtonView(title: .constant("="))
+          ButtonView(title: .constant("/"), value: $input)
+          ButtonView(title: .constant("0"), value: $input)
+          ButtonView(title: .constant("."), value: $input)
+          ButtonView(title: .constant("="), value: $input)
         }
       }
       .padding([.leading,.trailing], 20)
@@ -60,8 +61,8 @@ struct ContentView: View {
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-          .embedInNavigationView()
-    }
+  static var previews: some View {
+    ContentView()
+      .embedInNavigationView()
+  }
 }
