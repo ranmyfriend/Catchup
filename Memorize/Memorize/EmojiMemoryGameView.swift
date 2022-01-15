bfb3e9126a440cb829897ffd87d87d3effaaa7d1
Ranjith <Ranjith.Rajendiran-ext@bshg.com>
2022-01-15 13:39:51 +0530

    CS193p lecture 7 contd: - Cardify view modifier created and also added the existing functionality

--- Memorize/Memorize/EmojiMemoryGameView.swift
+++ Memorize/Memorize/EmojiMemoryGameView.swift
@@ -7,6 +7,9 @@
 
 import SwiftUI
 
+//STOPPED AT 15.06
+//Lecture 7
+
 struct EmojiMemoryGameView: View {
     /// @ObservedObject is a Property Wrapper and we have made the viewModel has Observed one. so it has some published object. So whenever there is some change in viewModel, automatically the view is going to recreated :) MVVM
     @ObservedObject var game: EmojiMemoryGame
@@ -25,7 +28,6 @@ struct EmojiMemoryGameView: View {
         })
             .foregroundColor(.red)
             .padding(.horizontal)
-            .navigationTitle("MemorizeApp")
     }
 }
 
@@ -35,20 +37,13 @@ struct CardView: View {
     var body: some View {
         GeometryReader { geometry in
             ZStack {
-                let shape = RoundedRectangle(cornerRadius: DrawingConstants.cornerRadius)
-                if card.isFaceUp {
-                    shape.fill().foregroundColor(.white)
-                    shape.strokeBorder(lineWidth: DrawingConstants.lineWidth)
-                    Pie(startAngle: Angle(degrees: 0-90), endAngle: Angle(degrees: 150-90))
-                        .padding(5)
-                        .opacity(0.5)
-                    Text(card.content).font(font(in: geometry.size))
-                } else if card.isMatched {
-                    shape.opacity(0) //Faded the view
-                } else {
-                    shape.fill()
-                }
+                Pie(startAngle: Angle(degrees: 0-90), endAngle: Angle(degrees: 150-90))
+                    .padding(5)
+                    .opacity(0.5)
+                Text(card.content)
+                    .font(font(in: geometry.size))
             }
+            .cardify(isFaceUp: card.isFaceUp)
         }
     }
     
@@ -57,8 +52,6 @@ struct CardView: View {
     }
     
     private struct DrawingConstants {
-        static let cornerRadius: CGFloat = 10
-        static let lineWidth: CGFloat = 3
         static let fontScale: CGFloat = 0.7
     }
 }
@@ -69,7 +62,5 @@ struct ContentView_Previews: PreviewProvider {
         game.choose(game.cards.first!)
         return EmojiMemoryGameView(game: game)
             .preferredColorScheme(.light)
-//        EmojiMemoryGameView(game: game)
-//            .preferredColorScheme(.dark)
     }
 }
