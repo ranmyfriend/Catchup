8d04e4e42d54835c2cdb0a5a57875f93689165de
Ranjith <Ranjith.Rajendiran-ext@bshg.com>
2022-01-17 15:51:20 +0530

    Lecture 7 completed: - Two cards matched the linear animation applied through implicit - Handled landscape and portrait issues of positions

--- Memorize/Memorize/EmojiMemoryGameView.swift
+++ Memorize/Memorize/EmojiMemoryGameView.swift
@@ -7,9 +7,6 @@
 
 import SwiftUI
 
-//STOPPED AT 55.41
-//Lecture 7
-
 struct EmojiMemoryGameView: View {
     /// @ObservedObject is a Property Wrapper and we have made the viewModel has Observed one. so it has some published object. So whenever there is some change in viewModel, automatically the view is going to recreated :) MVVM
     @ObservedObject var game: EmojiMemoryGame
@@ -42,19 +39,22 @@ struct CardView: View {
                     .opacity(0.5)
                 Text(card.content)
                     .rotationEffect(Angle(degrees: card.isMatched ? 360 : 0))
-                    .animation(Animation.easeIn(duration: 2))
-                    .font(font(in: geometry.size))
+                    .animation(Animation.linear(duration: 1)
+                                .repeatForever(autoreverses: false))
+                    .font(Font.system(size: DrawingConstants.fontSize))
+                    .scaleEffect(scale(thatFits: geometry.size))
             }
             .cardify(isFaceUp: card.isFaceUp)
         }
     }
     
-    private func font(in size: CGSize) -> Font {
-        Font.system(size: min(size.width, size.height) * DrawingConstants.fontScale)
+    private func scale(thatFits size: CGSize) -> CGFloat {
+        min(size.width, size.height) / (DrawingConstants.fontSize / DrawingConstants.fontScale)
     }
     
     private struct DrawingConstants {
         static let fontScale: CGFloat = 0.7
+        static let fontSize: CGFloat = 32
     }
 }
 
