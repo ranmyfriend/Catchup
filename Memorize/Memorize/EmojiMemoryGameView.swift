62c8ba0b50d8845a5d3f4de3ba8f7f6e7f730825
Ranjith <Ranjith.Rajendiran-ext@bshg.com>
2022-01-28 13:39:41 +0530

    Lecture 8 completed: - Learned Geometry effects how to do an animation - Rotation, Scale, Rotation3D

--- Memorize/Memorize/EmojiMemoryGameView.swift
+++ Memorize/Memorize/EmojiMemoryGameView.swift
@@ -122,12 +122,27 @@ struct EmojiMemoryGameView: View {
 struct CardView: View {
     let card: EmojiMemoryGame.Card
     
+    @State private var animatedBonusRemaining: Double = 0
+    
     var body: some View {
         GeometryReader { geometry in
             ZStack {
-                Pie(startAngle: Angle(degrees: 0-90), endAngle: Angle(degrees: (1-card.bonusRemaining)*360-90))
-                    .padding(5)
-                    .opacity(0.5)
+                Group {
+                    if card.isConsumingBonusTime {
+                        Pie(startAngle: Angle(degrees: 0-90), endAngle: Angle(degrees: (1-animatedBonusRemaining)*360-90))
+                            .onAppear {
+                                animatedBonusRemaining = card.bonusRemaining
+                                withAnimation(.linear(duration: card.bonusTimeRemaining)) {
+                                    animatedBonusRemaining = 0
+                                }
+                            }
+                    } else {
+                        Pie(startAngle: Angle(degrees: 0-90), endAngle: Angle(degrees: (1-card.bonusRemaining)*360-90))
+                        
+                    }
+                }
+                .padding(5)
+                .opacity(0.5)
                 Text(card.content)
                     .rotationEffect(Angle(degrees: card.isMatched ? 360 : 0))
                     .animation(Animation.linear(duration: 1)
