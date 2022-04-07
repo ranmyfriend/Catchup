e67549533280340e3614cfc697d492bae7658f35
Ranjith <Ranjith.Rajendiran-ext@bshg.com>
2022-04-07 19:29:22 +0530

    changes

--- UnitTest-Learning/ButtonTap/ButtonTapUITests/ButtonTapUITests.swift
+++ UnitTest-Learning/ButtonTap/ButtonTapUITests/ButtonTapUITests.swift
@@ -26,6 +26,7 @@ class ButtonTapUITests: XCTestCase {
         // UI tests must launch the application that they test.
         let app = XCUIApplication()
         app.launch()
+        app.tables
 
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
