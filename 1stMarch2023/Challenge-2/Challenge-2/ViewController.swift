7b437a30bb0b38f84c1c85536b057264695e4b6f
Ranjith <Ranjith.Rajendiran-ext@bshg.com>
2023-03-03 00:47:07 +0530

    Swift tips proj added

---
+++ 1stMarch2023/Challenge-2/Challenge-2/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  Challenge-2
+//
+//  Created by BKS-GGS on 03/03/23.
+//
+
+import UIKit
+
+/*              Swift Tips          */
+
+class A {
+    func f(value: Int = 10) {
+        print("A:\(value)")
+    }
+}
+
+class B: A {
+    override func f(value: Int = 100) {
+        print("B:\(value)")
+    }
+}
+
+class C: B {
+    override func f(value: Int = 1000) {
+        print("C:\(value)")
+    }
+}
+//What will be the output?
+
+class Car {
+    
+}
+
+extension Car {
+    //unless otherwise if you add @objc then only it will be added into vTable
+    @objc func fuelCapacity() -> Double {
+        fatalError()
+    }
+}
+
+class Tata: Car {
+     override func fuelCapacity() -> Double {
+         return 0.0
+    }
+}
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let c: A = C()
+        c.f()
+    }
+
+
+}
+
